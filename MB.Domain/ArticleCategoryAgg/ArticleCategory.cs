e2e26a75900208768922f9c9788b790a3d5e32dc
m.sboskabadi <m.sboskabadi@gmail.com>
2024-04-27 22:02:59 +0330

    Add project files.

---
+++ MB.Domain/ArticleCategoryAgg/ArticleCategory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB.Domain.ArticleCategoryAgg
+{
+    public class ArticleCategory
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public bool IsDeleted { get; private set; }
+        public DateTime CreationDate { get; private set; }
+
+        public ArticleCategory(string title)
+        {
+            Title = title;
+            IsDeleted = false;
+            CreationDate = DateTime.Now;    
+        }
+      
+    }
+}
